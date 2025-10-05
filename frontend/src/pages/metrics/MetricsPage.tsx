8a5e8746c00fefc0c8a99504d43cc592cf68395c
Yaroslav <yaroslavkoshylko@gmail.com>
2025-10-05 17:12:24 +0300

    feat: metrics to excel

--- frontend/src/pages/metrics/MetricsPage.tsx
+++ frontend/src/pages/metrics/MetricsPage.tsx
@@ -5,6 +5,8 @@ import PermutationsTable from "../../components/permutationsTable/PermutationsTa
 import { useAlbumsStore } from "../../hooks/stores/albumStore";
 import PermutationsFooter from "../../components/permutationsFooter/PermutationsFooter";
 
+import * as XLSX from "xlsx";
+
 const MetricsPage = () => {
    const cookMetrics = useMetricsStore((state) => state.cookMetrics);
    const albums = useAlbumsStore((state) => state.albums);
@@ -16,6 +18,33 @@ const MetricsPage = () => {
       fetchAlbums();
       console.log(cookMetrics);
    }, []);
+
+   useEffect(() => {
+      if (!cookMetrics || !albums.length) return;
+
+      const headers = [
+         ...albums.map((album) => album.title),
+         ...cookMetrics.permutationResults[0].distances.map((d) => d.user),
+         "Sum",
+         "Max",
+      ];
+
+      const data: (string | number)[][] = cookMetrics.permutationResults.map((p) => [
+         ...p.permutation,
+         ...p.distances.map((d) => d.distance),
+         p.sum,
+         p.maxl,
+      ]);
+
+      data.unshift(headers);
+
+      const ws = XLSX.utils.aoa_to_sheet(data);
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, "CookMetrics");
+
+      XLSX.writeFile(wb, "cook_metrics.xlsx");
+   }, [cookMetrics, albums]);
+
    return (
       <>
          <section className="metrics-page">
